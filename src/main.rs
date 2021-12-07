617ccbd1e75ad2505c46167ffe3d309bea9afb5b
Corné <code@jammo.nl>
2021-12-07 20:35:27 +0100

    Refactor main, allow to run all days and made adding days easier

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::time::Instant;
+use std::collections::BTreeMap;
 mod tools;
 
 mod day01;
@@ -13,33 +14,46 @@ mod day07;
 fn main() {
     let now = Instant::now();
 
+    let days = BTreeMap::from([
+        ("day01", day01::run_all as for<'r> fn(&'r str) -> String),
+        ("day02", day02::run_all),
+        ("day03", day03::run_all),
+        ("day04", day04::run_all),
+        ("day05", day05::run_all),
+        ("day06", day06::run_all),
+        ("day07", day07::run_all),
+    ]);
+
     let args: Vec<String> = env::args().collect();
-    let mut input: String = "".into();
+    let mut input: &str = "";
     let mut day = "";
 
     if args.len() > 1 {
         day = &args[1];
-        input = format!("./inputs/{}.txt", day);
+        input = "./inputs/{DAY}.txt";
     }
     if args.len() > 2 {
         // if we have a argument, use the testfile, this way I can
         // switch between the real and test file for development / test puproses
-        input = format!("./inputs/{}_test.txt", day);
+        input = "./inputs/{DAY}_test.txt";
     }
 
     println!("running with file {}", input);
 
-    let runner = match day {
-        "day01" => day01::run_all(&input),
-        "day02" => day02::run_all(&input),
-        "day03" => day03::run_all(&input),
-        "day04" => day04::run_all(&input),
-        "day05" => day05::run_all(&input),
-        "day06" => day06::run_all(&input),
-        "day07" => day07::run_all(&input),
-        _ => panic!("Incorrect input")
-    };
 
-    println!("\nruntime {} ms", now.elapsed().as_millis());
-    println!("{}", runner);
+    let mut result: String = "".into();
+    if day == "all" {
+        for (day, func) in days {
+            let file = input.to_string().replace("{DAY}", day);
+            println!("running day {} with file {}", day, file);
+            result = format!("{}\n{}:{}", result, day, func(&file));
+        }
+    } else {
+        let file = input.to_string().replace("{DAY}", day);
+        println!("running day {} with file {}", day, file);
+        result = days[day](&file);
+    }
+
+    println!("{}", result);
+    println!("Finished in {} ms\n", now.elapsed().as_millis());
 }
