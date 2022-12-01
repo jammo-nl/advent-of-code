5aa19b4b2c458ac4e653d40ea180994d43451f75
Corné <46375984+jammo-nl@users.noreply.github.com>
2022-12-01 16:12:50 +0100

    Base setup for running AoC code

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub fn noop(_inp: String) {}
+
+pub type DayFn = fn(String);
+
+pub fn get_day(day: u32) -> (DayFn, DayFn) {
+    return match day {
+        _ => {
+            println!("Unknown day: {}", day);
+            return (noop, noop);
+        }
+    };
+}
