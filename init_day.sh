f22dab5c1d357d8171eabe4a4a7efa0e03373938
Corné <code@jammo.nl>
2023-12-01 00:20:59 +0100

    Add init scripts for new year/day and some basic templates

---
+++ init_day.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+SCRIPT_DIR=$(realpath "$(dirname "$0")")
+
+if [[ $# != 2 ]]; then
+  echo "Please provide a year and day number."
+  echo "usage: $0 YEAR DAY"
+  exit 1
+fi
+
+if [[ ! "$1" =~ ^20([1-9]{2,})$ ]]; then
+  echo "Argument '$1' is not a valid year."
+  exit 1
+fi
+
+if [[ ! "$2" =~ ^(0[1-9]|1[0-9]|2[0-5])$ ]]; then
+  echo "Argument '$2' is not a valid day."
+  exit 1
+fi
+
+if [[ -z "${AOC_SESSION-""}" ]]; then
+  echo "No session token set in \$AOC_SESSION."
+  exit 1
+fi
+
+## copy init templates
+cp templates/day.rs aoc$1/src/bin/day$2.rs
+
+## download input files
+AOC_SESSION=$AOC_SESSION ./fetch.sh $1 $2
+
---
+++ init_year.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+SCRIPT_DIR=$(realpath "$(dirname "$0")")
+
+if [[ $# != 1 ]]; then
+  echo "Please provide a year."
+  echo "usage: $0 YEAR"
+  exit 1
+fi
+
+if [[ ! "$1" =~ ^20([1-9]{2,})$ ]]; then
+  echo "Argument '$1' is not a valid year."
+  exit 1
+fi
+
+cd $SCRIPT_DIR
+cargo init "aoc$1"
+mkdir -p "aoc$1/src/bin"
+
+## copy init templates
+cp templates/Cargo.toml aoc$1/Cargo.toml
+cp templates/lib.rs aoc$1/src/lib.rs
+cp templates/main.rs aoc$1/src/main.rs
+sed -i "s/{YEAR}/$1/" aoc$1/Cargo.toml
+
