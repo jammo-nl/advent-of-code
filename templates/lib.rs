4d88a45383add2b85e78732373ecbef384756e10
Corné <code@jammo.nl>
2023-12-01 11:19:37 +0100

    Update macro to add the input file

--- templates/lib.rs
+++ templates/lib.rs
@@ -1,18 +1,22 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use quote::{quote, ToTokens};
+use syn::{parse_macro_input, Expr, ItemFn};
 
 #[proc_macro_attribute]
-pub fn main(args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn main(attr: TokenStream, item: TokenStream) -> TokenStream {
     // parse input
+    let attr = parse_macro_input!(attr as Expr);
+    let day_attr = attr.to_token_stream().to_string();
+    let input_file = format!("../../inputs/day{}.txt", day_attr);
 
-    let mut solution = parse_macro_input!(input as ItemFn);
+    let solution = parse_macro_input!(item as ItemFn);
 
     let tokens = quote! {
+        const INPUT: &str = include_str!(#input_file);
         #solution
         fn main() {
             // get file content and add timing
-            let (p1, p2) = solution("");
+            let (p1, p2) = solution(INPUT);
             println!("Part one: {}", p1);
             println!("Part two: {}", p2);
       }
