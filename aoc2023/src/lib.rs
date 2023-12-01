32f13937d39e32eed92de5f52621e0ca15e0ef20
Corné <46375984+jammo-nl@users.noreply.github.com>
2023-12-01 15:53:31 +0100

    Added per day timing

--- aoc2023/src/lib.rs
+++ aoc2023/src/lib.rs
@@ -13,10 +13,37 @@ pub fn main(attr: TokenStream, item: TokenStream) -> TokenStream {
 
     let tokens = quote! {
         const INPUT: &str = include_str!(#input_file);
+
+        use std::time::{Duration, Instant};
+
+        struct MeasureTime {
+            start: Instant,
+        }
+        impl MeasureTime {
+            pub fn start() -> Self {
+                Self {
+                    start: Instant::now(),
+                }
+            }
+            pub fn duration(&self) -> String {
+                let duration = self.start.elapsed();
+                if duration < Duration::from_millis(1) {
+                    format!("{}μs", duration.as_micros())
+                } else if duration < Duration::from_secs(1) {
+                    format!("{:.3}ms", duration.as_micros() as f64 / 1000.0)
+                } else {
+                    format!("{:.3}s", duration.as_millis() as f64 / 1000.0)
+                }
+            }
+        }
+
         #solution
         fn main() {
+            let total = MeasureTime::start();
             // get file content and add timing
             let (p1, p2) = solution(INPUT.trim());
+            println!("Finished in {}", total.duration());
+
             println!("Part one: {}", p1);
             println!("Part two: {}", p2);
       }
