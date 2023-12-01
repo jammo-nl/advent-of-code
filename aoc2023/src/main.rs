bf1eaf70fd4d9a5eaddd560a00d13e913215ce24
Corné <46375984+jammo-nl@users.noreply.github.com>
2023-12-01 13:29:33 +0100

    Day 01 solution

---
+++ aoc2023/src/main.rs
@@ -0,0 +1,41 @@
+use std::process::Command;
+
+fn main() {
+    let paths = std::fs::read_dir(format!("{}/src/bin/", env!("CARGO_MANIFEST_DIR"))).unwrap();
+    let mut run = vec![];
+
+    for path in paths {
+        run.push(
+            path.unwrap()
+                .path()
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .replace(".rs", ""),
+        );
+    }
+
+    for day in run {
+        let cmd = Command::new("cargo")
+            .args(["run", "--release", "--bin", &day])
+            .output()
+            .unwrap();
+
+        println!("----------------------------------------------");
+        println!("|              {}                         |", day);
+        println!("----------------------------------------------");
+
+        let output = String::from_utf8(cmd.stdout).unwrap();
+        let is_empty = output.is_empty();
+
+        println!(
+            "{}",
+            if is_empty {
+                "Not solved."
+            } else {
+                output.trim()
+            }
+        )
+    }
+}
