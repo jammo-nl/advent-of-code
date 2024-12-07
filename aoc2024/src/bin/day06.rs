00257276edde2b3baa9abe3667cdfe9e594c3959
Corné <code@jammo.nl>
2024-12-07 03:03:09 +0100

    Day 06 part2 (bug, wrong output...)

--- aoc2024/src/bin/day06.rs
+++ aoc2024/src/bin/day06.rs
@@ -1,16 +1,30 @@
+use std::collections::HashSet;
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 enum Item {
-    Space(Vec<isize>),
+    Space(bool),
     Wall,
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
+enum ReturnType {
+    Working,
+    OutOfBound,
+    Loop,
+}
+
 fn step_trough(
+    start_pos: isize,
     pos: &mut isize,
     current_dir: &mut u8,
     directions: &Vec<isize>,
     grid: &mut Vec<Item>,
     width: isize,
-) -> bool {
+    visits: &mut HashSet<(isize, u8)>,
+    tested_walls: &mut HashSet<isize>,
+    loop_test: bool,
+    loop_count: &mut usize,
+) -> ReturnType {
     let start_x = *pos % width;
     let start_y = *pos / width;
     let test_pos = *pos + directions[*current_dir as usize];
@@ -19,20 +33,86 @@ fn step_trough(
     let end_y = test_pos / width;
 
     if (end_x != start_x && end_y != start_y) || test_pos < 0 || test_pos >= grid.len() as isize {
-        println!("out of bound, we are done..");
-        return false;
+        return ReturnType::OutOfBound;
     }
 
-    let total_len = grid.len();
+    // collect info for a test round
+    let mut do_loop_test = false;
     match &mut grid[test_pos as usize] {
-        Item::Space(count) => {
-            // update our position
+        Item::Space(res) => {
+            do_loop_test = true;
+            // check if we are looping
+            if visits.contains(&(test_pos, *current_dir)) {
+                *loop_count += 1;
+                return ReturnType::Loop;
+            }
+
             *pos = test_pos;
-            count.push(*pos);
+            // keep track of visited locations for the main loop only
+            if loop_test {
+                *res = true;
+            }
+            visits.insert((*pos, *current_dir));
         }
         Item::Wall => *current_dir = (*current_dir + 1) % directions.len() as u8,
     }
-    true
+
+    if !(loop_test && do_loop_test) {
+        return ReturnType::Working;
+    }
+    // check if the next value is blocking, a wall or our start location, if so we can not build an obstacle
+    let obs_pos = *pos + directions[*current_dir as usize];
+    let obs_x = obs_pos % width;
+    let obs_y = obs_pos / width;
+
+    if (obs_x != end_x && obs_y != end_y) || obs_pos < 0 || obs_pos > grid.len() as isize {
+        //println!("Done, not placing an obstacle outside of the world");
+        return ReturnType::Working;
+    }
+    if obs_pos == start_pos {
+        return ReturnType::Working;
+    }
+    if let Item::Wall = grid[obs_pos as usize] {
+        //println!("Done, not placing an obstacle on a wall");
+        return ReturnType::Working;
+    }
+    if tested_walls.contains(&obs_pos) {
+        return ReturnType::Working;
+    }
+
+    // clone the grid and add a wall for this loop
+    let mut tst_grid = grid.clone();
+    tst_grid[obs_pos as usize] = Item::Wall;
+    tested_walls.insert(obs_pos);
+
+    // track visits
+    let mut sub_visits = visits.clone();
+    let mut alt_pos = *pos;
+    let mut alt_dir = (*current_dir + 1) % directions.len() as u8;
+
+    loop {
+        let result = step_trough(
+            start_pos,
+            &mut alt_pos,
+            &mut alt_dir,
+            &directions,
+            &mut tst_grid,
+            width,
+            &mut sub_visits,
+            tested_walls,
+            false,
+            loop_count,
+        );
+
+        if result == ReturnType::OutOfBound {
+            break;
+        } else if result == ReturnType::Loop {
+            return ReturnType::Loop;
+        } else {
+        }
+    }
+
+    ReturnType::Working
 }
 
 #[aoc2024::main(06)]
@@ -42,16 +122,16 @@ fn solution(input: &str) -> (usize, usize) {
     // populate the grid
     let width = input.lines().next().unwrap().len() as isize;
     let directions: Vec<isize> = vec![-1 * width, 1, 1 * width, -1];
+    let mut visits = HashSet::new();
+    let mut tested_walls = HashSet::new();
 
     let mut grid: Vec<Item> = input
         .lines()
-        .enumerate()
-        .flat_map(|(c, i)| {
+        .flat_map(|i| {
             i.chars()
-                .enumerate()
-                .map(|(c2, j)| match j {
-                    '.' => Item::Space(vec![]),
-                    '^' => Item::Space(vec![(c + c2) as isize]),
+                .map(|j| match j {
+                    '.' => Item::Space(false),
+                    '^' => Item::Space(true),
                     '#' => Item::Wall,
                     _ => panic!("no such item"),
                 })
@@ -63,35 +143,36 @@ fn solution(input: &str) -> (usize, usize) {
     let mut pos = *grid
         .iter()
         .enumerate()
-        .filter(|(_, item)| {
-            if let Item::Space(items) = item {
-                items.len() > 0
-            } else {
-                false
-            }
-        })
+        .filter(|(_, item)| **item == Item::Space(true))
         .map(|(idx, _)| idx)
         .collect::<Vec<usize>>()
         .first()
         .unwrap() as isize;
 
-    while step_trough(&mut pos, &mut current_dir, &directions, &mut grid, width) {
-        //
-    }
+    let mut loop_count = 0;
+
+    let start_pos = pos;
+    while step_trough(
+        start_pos,
+        &mut pos,
+        &mut current_dir,
+        &directions,
+        &mut grid,
+        width,
+        &mut visits,
+        &mut tested_walls,
+        true,
+        &mut loop_count,
+    ) != ReturnType::OutOfBound
+    {}
 
     let total_1 = grid
         .iter()
-        .filter(|item| {
-            if let Item::Space(items) = item {
-                items.len() > 0
-            } else {
-                false
-            }
-        })
+        .filter(|item| **item == Item::Space(true))
         .collect::<Vec<_>>()
         .len();
 
-    (total_1, 0)
+    (total_1, loop_count)
 }
 
 #[cfg(test)]
